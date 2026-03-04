39d6a0ab12495a5e0c2dec045450ca3838ecf811
EvanKhanDev <evandesarrollo@gmail.com>
2026-03-03 21:27:43 -0600

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-
 echo "Enter principal:"
 read principal
-
 echo "Enter rate of interest:"
 read rate
-
 echo "Enter time period:"
 read time
-
 interest=$((principal * rate * time / 100))
-
-echo "Simple Interest = $interes"
+echo "Simple Interest = $interest"
